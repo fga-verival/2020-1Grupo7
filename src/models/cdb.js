a3c9e75a26473d3918ed4b2a996c22774ce89b9d
Luis Taira <luishptaira@gmail.com>
2020-11-01 16:55:33 -0300

    a) falsificacao

--- src/models/cdb.js
+++ src/models/cdb.js
@@ -1,7 +1,17 @@
 class Cdb {
     constructor() { }
 
+    setAplicacaoInicial(number) {
 
+    }
+
+    setDias(number) {
+
+    }
+
+    getRendimentoBruto() {
+        return 13.15
+    }
 }
 
 module.exports = Cdb;
