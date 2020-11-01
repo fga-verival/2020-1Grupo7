72789e79a636228377fc0f7cdc441b228fa33b1c
murilo-dan <murilo.loiola.dan@gmail.com>
2020-11-01 19:09:36 -0300

    b) barra verde por falsificação

--- src/models/cdb.js
+++ src/models/cdb.js
@@ -1,18 +1,26 @@
 class Cdb {
-    constructor(i) { 
-    	this.i = i/100;
+    constructor(i) {
+        this.i = i / 100;
     }
 
     setAplicacaoInicial(number) {
-    	this.aplicacaoInicial = number;
+        this.aplicacaoInicial = number;
     }
 
     setDias(number) {
-    	this.dias = number;
+        this.dias = number;
+    }
+
+    setAliquota(number) {
+
     }
 
     getRendimentoBruto() {
-        return parseFloat((this.aplicacaoInicial * this. i * this.dias/365).toFixed(2));
+        return parseFloat((this.aplicacaoInicial * this.i * this.dias / 365).toFixed(2));
+    }
+
+    getImpostoDeRenda() {
+        return 3.14;
     }
 }
 
