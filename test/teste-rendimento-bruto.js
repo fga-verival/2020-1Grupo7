bc078629ace12109ff16bed7edc4773986d1cdf8
Sam Buters <sambuters@gmail.com>
2020-11-01 17:25:39 -0300

    a) barra vermelha novo teste

--- test/teste-rendimento-bruto.js
+++ test/teste-rendimento-bruto.js
@@ -4,10 +4,17 @@ const Cdb = require('../src/models/cdb.js')
 describe('Rendimento bruto', function() {
   describe('getRendimentoBruto()', function() {
     it('Deve retornar 13.15', function() {
-      const aplicacaoCdb = new Cdb;
+      const aplicacaoCdb = new Cdb(8);
       aplicacaoCdb.setAplicacaoInicial(500.00);
       aplicacaoCdb.setDias(120);
       assert.equal(aplicacaoCdb.getRendimentoBruto(), 13.15);
+    });
+
+    it('Deve retornar 23.01', function() {
+      const aplicacaoCdb = new Cdb(8);
+      aplicacaoCdb.setAplicacaoInicial(250.00);
+      aplicacaoCdb.setDias(420);
+      assert.equal(aplicacaoCdb.getRendimentoBruto(), 23.01);
     });
   });
 });
